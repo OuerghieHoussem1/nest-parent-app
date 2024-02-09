924b3b97511dd6264726ef961b4cac9d710692fd
Ouerghie Houssem <126062032+OuerghieHoussem1@users.noreply.github.com>
2024-02-09 17:00:46 +0100

    initializing microservices

--- apps/cats/src/main.ts
+++ apps/cats/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
+import { CatsModule } from './cats.module';
 import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
+  const app = await NestFactory.create(CatsModule);
   app.connectMicroservice({
     transport:Transport.TCP,
     options:{
-      port:3001
+      port:3002
     }
   })
   await app.startAllMicroservices()
-  await app.listen(3001);
+  await app.listen(3002);
 }
 bootstrap();
--- apps/dogs/src/main.ts
+++ apps/dogs/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
+import { DogsModule } from './dogs.module';
 import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
+  const app = await NestFactory.create(DogsModule);
   app.connectMicroservice({
     transport:Transport.TCP,
     options:{
-      port:3001
+      port:3003
     }
   })
   await app.startAllMicroservices()
-  await app.listen(3001);
+  await app.listen(3003);
 }
 bootstrap();
