c5a9831a2666c990ae941cac6ebfba869efe0b96
Ouerghie Houssem <126062032+OuerghieHoussem1@users.noreply.github.com>
2024-02-09 17:03:17 +0100

    added some identification text

--- apps/cats/src/cats.service.ts
+++ apps/cats/src/cats.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CatsService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World from cats!';
   }
 }
--- apps/dogs/src/dogs.service.ts
+++ apps/dogs/src/dogs.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class DogsService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World from dogs!';
   }
 }
